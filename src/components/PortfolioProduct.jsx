40792e1cc631944bc1cc79e95984eba267fa24eb
KumarRahul01 <kumar.coding.practice@gmail.com>
2024-05-08 23:18:13 +0530

    commit

--- src/components/PortfolioProduct.jsx
+++ src/components/PortfolioProduct.jsx
@@ -4,7 +4,7 @@ import { FaLink } from "react-icons/fa6";
 
 const PortfolioProduct = ({image}) => {
   return (
-    <div className="w-[26rem] overflow-hidden hover:relative prodImage hover:cursor-pointer hover:text-orange-600">
+    <div className="w-[26rem] overflow-hidden hover:relative hover:cursor-pointer hover:text-orange-600">
       <img
         src={image}
         className="w-fit h-fit object-cover transition-all duration-300 hover:opacity-60 hover:scale-110"
