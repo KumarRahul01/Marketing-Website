9fe3150351de80a8bac5e39a3abb98c49a961cd9
KumarRahul01 <kumar.coding.practice@gmail.com>
2024-07-05 10:19:29 +0530

    Final

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
     <div className="w-full xl:h-24 lg:h-20 h-16 flex flex-row items-center justify-between lg:px-32 sm:px-20 px-10 shadow-lg relative secondary-font">
       {/* logo */}
       <a
-        href="/"
+        href="/Marketing-Website/"
         className="lg:text-3xl text-2xl tracking-wide font-light text-[#7A6960]"
       >
         BrandName
@@ -23,7 +23,7 @@ const NavBar = () => {
       {/* menu */}
       <div className="lg:flex items-center justify-between lg:gap-14 hidden">
         <a
-          href="/"
+          href="/Marketing-Website/"
           className="text-gray-800 hover:text-[#EE7540] text-lg tracking-wide font-light"
         >
           Home
@@ -71,7 +71,7 @@ const NavBar = () => {
       {show && (
         <div className="absolute z-20 top-[70px] flex flex-col gap-4 text-center w-full left-0 p-5 lg:hidden shadow-xl bg-white transition-all duration-300">
           <a
-            href="/"
+            href="/Marketing-Website/"
             className="text-gray-800 hover:text-[#EE7540] text-lg tracking-wide font-light"
           >
             Home
